4cc953714a0a323d72990d79053c09652645fa75
FrogUni <80472206+danpung628@users.noreply.github.com>
2025-03-31 13:24:21 +0900

    3/31 모프 실습

--- eweek04a/src/main/java/com/example/eweek04a/MainActivity.kt
+++ eweek04a/src/main/java/com/example/eweek04a/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.eweek04a
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,18 +11,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.eweek04a.ui.theme.MylecETheme
+import com.example.eweek04a.uicomponents.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // enableEdgeToEdge() 상단 바
         setContent {
             MylecETheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
---
+++ eweek04a/src/main/java/com/example/eweek04a/uicomponents/MainScreen.kt
@@ -0,0 +1,20 @@
+package com.example.eweek04a.uicomponents
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.example.eweek04a.model.TodoItemFactory
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+val todoList = remember {
+    //mutableStateListOf<Item>()
+    TodoItemFactory.makeTodoList()
+}
+    Column {
+        TodoListTitle()
+        TodoList(todoList)
+        TodoItemInput(todoList)
+    }
+}
