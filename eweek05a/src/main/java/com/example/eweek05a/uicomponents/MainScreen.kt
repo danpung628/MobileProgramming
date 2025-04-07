c5b40035f5a14e53fb0197de65dd5e8b50f742d7
FrogUni <80472206+danpung628@users.noreply.github.com>
2025-04-07 13:27:08 +0900

    04/07 실습

---
+++ eweek05a/src/main/java/com/example/eweek05a/uicomponents/MainScreen.kt
@@ -0,0 +1,35 @@
+package com.example.eweek05a.uicomponents
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import com.example.eweek05a.R
+import com.example.eweek05a.model.ButtonType
+import com.example.eweek05a.model.ImageData
+import com.example.eweek05a.model.ImageUri
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    val img1State by rememberSaveable(stateSaver = ImageData.ImageSaver) {
+        mutableStateOf(
+            ImageData(
+                image = ImageUri.ResImage(R.drawable.img1),
+                buttonType = ButtonType.BADGE,
+                likes = 50
+            )
+        )
+    }
+
+    val img2State by rememberSaveable(stateSaver = ImageData.ImageSaver) {
+        mutableStateOf(
+            ImageData(
+                image = ImageUri.ResImage(R.drawable.img2),
+                buttonType = ButtonType.EMOJI,
+                likes = 40,
+                dislikes = 10
+            )
+        )
+    }
+}
