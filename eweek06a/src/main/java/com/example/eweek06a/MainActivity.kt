fc2acc1cdf728323a93aa2318a39a142dc8d93c2
FrogUni <80472206+danpung628@users.noreply.github.com>
2025-04-09 22:59:39 +0900

    04/09 실습 완료

---
+++ eweek06a/src/main/java/com/example/eweek06a/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.eweek06a
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.eweek06a.ui.theme.MylecETheme
+import com.example.week06.uicomponents.MainScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MylecETheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MylecETheme {
+        Greeting("Android")
+    }
+}
